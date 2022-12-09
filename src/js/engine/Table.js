af2b5ad339a142d61b740fd391e6fdcb9d7fa7e4
laps78 <9254384@mail.ru>
2022-12-09 23:12:42 +0300

    class Table is written & expects to be tested out

--- src/js/engine/Table.js
+++ src/js/engine/Table.js
@@ -4,4 +4,27 @@ export default class Table {
     this.cards = collection.cards;
     this.used = [];
   }
+
+  /**
+   * this method mixes cards in random order
+   */
+  mixCards() {
+    this.cards = this.cards.sort(() => Math.random() - 0.5);
+  }
+
+  /**
+   * throws the card out of active collection
+   * @param {*} card is a card to throw out
+   */
+  throwCard(card) {
+    this.cards.splice(this.cards.indexOf(card), 1);
+    this.used.push(card);
+  }
+
+  /**
+   * this method restores cards
+   */
+  restoreCards() {
+    this.cards = this.collection.cards;
+  }
 }
