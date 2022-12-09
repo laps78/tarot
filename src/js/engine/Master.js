ab4997d4e897b8d56f381e3000b02af4d7159dda
laps78 <9254384@mail.ru>
2022-12-09 23:21:14 +0300

    engine configuration beta impressed...

---
+++ src/js/engine/Master.js
@@ -0,0 +1,30 @@
+export default class Master {
+  constructor(collection) {
+    this.collection = collection;
+    this.cards = collection.cards;
+    this.used = [];
+  }
+
+  /**
+   * this method mixes cards in random order
+   */
+  mixCards() {
+    this.cards = this.cards.sort(() => Math.random() - 0.5);
+  }
+
+  /**
+   * throws the card out of active collection
+   * @param {*} card is a card to throw out
+   */
+  throwCard(card) {
+    this.cards.splice(this.cards.indexOf(card), 1);
+    this.used.push(card);
+  }
+
+  /**
+   * this method restores cards
+   */
+  restoreCards() {
+    this.cards = this.collection.cards;
+  }
+}
